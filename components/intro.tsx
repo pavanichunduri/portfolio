ab8455dd5c59e08d91066e1c83e74f33b58558d1
pavanichunduri <pavanichunduri5028@gmail.com>
2024-05-22 08:08:50 -0700

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Pavani.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">1 year</span> of experience. I enjoy
+        <span className="font-bold">2+ year</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. 
       </motion.h1>
 
